23dd57a4d2a49cead0e55fbb4f326ca9d7c79e61
24eg105g18-cpu <24eg105g18@anurag.edu.in>
2026-05-11 14:53:32 +0530

    Delete counters directory

--- counters/src/stores/useCounterStore.js
+++
@@ -1,9 +0,0 @@
-import {create} from 'zustand'
-export const useCounterStore=create((set)=>({
-    //create a state
-    newCounter:0,
-    //functions to modify the state
-    incrementCounter:()=>set(state=>({newCounter:state.newCounter+1})),
-    decrementCounter:()=>set(state=>({newCounter:state.newCounter-1})),
-    resetCounter:()=>set({newCounter:0})
-}));
